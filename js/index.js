77210fdbb6fe3a459adafc5ad8381f5e058b5f40
srcNela <165686420+srcNela@users.noreply.github.com>
2024-12-04 20:54:18 -0600

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,36 @@
+let literales;
+
+literales.notas.unAtardecerNoCumplido = `  Donde se ha ido la ilusión de un momento importante, sea o no para el otro, pero que en el fondo se puede hacer significativo.
+Estando en el dualismo, viendo como las oportunidades que suceden ante ti son valoradas esos momentos que no se sabe, pero que la experiencia los recubre, les da una probabilidad 
+un valor con el cual, uno se permite el convencerse para no vivirlos. En mi mente me he doblado, imaginando estar ahí, algo que solo hace un masaje mental una excusa fácil de 
+la imaginación para no sentirse mal, pero, por otro lado, cuando dejas de creer en esa fantasía, vuelve el recaer en que se debería estar ahí, en un día importante o como en 
+otros ya pasados. Acaso ya no entiendo o ya no quiero entender el alrededor, me quedo en un cubículo el cual me permite seguir en la reflexión alimentando mis idílicas ideas.
+Veo y me observo de lo que puedo dar, de lo que soy capaz, en el fondo todo el ser que forma parte de este mundo puede, es cuestión de querer, pero cuando dejé de creer en 
+querer, acaso ya no sé amar o solo un tiempo breve, un tiempo determinado el cual está sujeto a unas necesidades. Caigo en mis pensamientos, donde en el suelo veo que quiero
+pero al levantarme, queda el mundo de probabilidades generado esa estupenda experiencia que hace que prescindas de cosas hermosas o por vivir solo por el hecho de recurrir a
+lo seguro. Me siento un mal amigo, alguien que ya no se puede contar con él, alguien que demuestra que se encierra despreocupando sé del alrededor, sin embargo, hay 
+contradicciones ese dualismo que provoca el bloqueo el sentir que se está en el error, en el constante querer corregir. Sin saber en el que sucederá, vivo con la experiencia 
+de las amistades temporales, largas o breves que en la corta vida que tengo he tenido, en esa atemporalidad de momentos en como surgieron y duraron o en como cambiaron 
+y terminaron. Sea por los lugares donde he vivido o por el modo de socializar, pero estos van evolucionando, cambiando. Ante todo subyace un valor, el valor de amar, de ser 
+alguien con quien confiar, sin embargo, en esas palabras, a la vez subyace el que donde está el verdadero o la síntesis del ser amistoso. Me gustaría cambiarlo, volver a ser 
+un amigo con el que se puede contar, pero así como en otros aspectos, no me veo ya preparado, o más bien ando cansado en el ego, en una burbuja de recursos seguros, en una 
+rutina que aunque contradictoria me hace volver. ¿En el fondo puede que no quiera ya afrontar amistades, esa responsabilidad por no estar seguro de poder estar ahí? Esta y 
+otras preguntas se pueden comparar, todo recae en lo mismo, el amor podría conseguirlo, pero temo no estar, no poder dar de mí lo que se necesita, aunque eso debería 
+salir solo. 
+En las amistades, he recurrido o más bien ya solo me acompaña la soledad, pero provocado por mí mismo, por ese quizás egocentrismo autofrustrado que me envuelve en las posibles 
+probabilidades imaginarias. Quizás sea mejor así, en la pareja, que tanto añoro, quizás no podría hacerla feliz aunque sé que podría, pero como demostrarme a mí que puedo
+ser capaz… 
+En las amistades que podría tener y tengo, el ver como luchan y yo pierdo el tiempo o me sumerjo en la facilidad de soñar en el mañana mientras veo pasar el presente. Estando 
+con los demás me siento capaz, pero al final en este “yin yang” termina el turno y vuelvo a mí. Vuelvo a mí, porque un día dije que quería ser capaz por mí mismo, no obstante 
+solo pienso en como sería haciéndolo con los demás. En ese atardecer que hoy podría haber pasado ahí, jugando al ajedrez, conociendo a mi oponente y reír, esperar a que se 
+oscureciera para poder felicitar a un amigo en un día que puede o no ser importante para él, pero que yo he decidido dejar de valorar. En ese posible camino de conocer la locura 
+junto a una chica, pero dejé de andar o simplemente el haberme quedado sentado junto a ellos, haciendo cualquier cosa que necesitaran o simplemente estando en el presente 
+compartiendo ese proyecto. Me veo envuelto otra vez de mi ser, de no comprender o comprendiendo que elijo lo que luego hace sentirme culpable. Ese malestar de estar expresando 
+en palabras en vez de sentir con miradas, gestos o hablando. Estoy agotado de no intentarlo, de no vivirlo, pero a la vez estoy agotado de las probabilidades de intentarlo. 
+El afrontar retos nuevos, el ser capaz de llevarlos a cabo, así como en él divague de querer aprenderlo todo, como puedo hacer para centrarme en algo. Quedándome en la superficie 
+de los sueños y vivencias, abstrayéndome en el sueño de la realidad.`;
+
+let elText = document.createElement("div");
+elText.innerText = literales.notas.unAtardecerNoCumplido;
+document.body.appendChild(elText);
+
